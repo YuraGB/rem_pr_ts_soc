b6c20dffcaf83971fbe8291b764a569756dbc312
yuhur <yuhur1985@gmail.com>
2023-10-20 18:12:02 +0300

    sockets

--- app/root.tsx
+++ app/root.tsx
@@ -12,11 +12,8 @@ import {
 import { NextUIProvider } from "@nextui-org/react";
 import stylesheet from "./tailwind.css";
 import type { ReactElement } from "react";
-<<<<<<< Updated upstream
 import { json, LoaderFunctionArgs } from "@remix-run/node";
 import { authenticator } from "~/server/auth";
-=======
->>>>>>> Stashed changes
 
 export const links: LinksFunction = () => [
   ...(cssBundleHref
@@ -25,10 +22,7 @@ export const links: LinksFunction = () => [
 ];
 
 export default function App(): ReactElement {
-<<<<<<< Updated upstream
   const user = useLoaderData();
-=======
->>>>>>> Stashed changes
   return (
     <html lang="en">
       <head>
